9372d12d4bbec39cdae38f8fd18a5ce3da57b479
santh-cpu <srisanth.p.a@gmail.com>
2026-02-08 21:06:52 +0530

    go linter persecuted

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,10 +1,9 @@
 return {
-  -- Mason
   {
     "williamboman/mason.nvim",
     config = true,
   },
-  -- Mason LSP bridge
+
   {
     "williamboman/mason-lspconfig.nvim",
     dependencies = { "mason.nvim" },
@@ -12,51 +11,63 @@ return {
       ensure_installed = {
         "pyright",
         "rust_analyzer",
-        "ts_ls",
+        "tsserver",
         "jdtls",
         "clangd",
         "gopls",
       },
     },
   },
-  -- Native LSP
+
   {
     "neovim/nvim-lspconfig",
     config = function()
-      -- UI polish
+      -- go-only diagnostic filter
+      local orig = vim.lsp.handlers["textDocument/publishDiagnostics"]
+      vim.lsp.handlers["textDocument/publishDiagnostics"] = function(err, result, ctx, config)
+        if ctx and ctx.bufnr then
+          local ft = vim.bo[ctx.bufnr].filetype
+          if ft == "go" and result and result.diagnostics then
+            result.diagnostics = vim.tbl_filter(function(d)
+              return d.code ~= "ST1003" and d.code ~= "ST1005"
+            end, result.diagnostics)
+          end
+        end
+        return orig(err, result, ctx, config)
+      end
+
       vim.lsp.handlers["textDocument/hover"] =
         vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 
       vim.lsp.handlers["textDocument/signatureHelp"] =
         vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
-      -- On attach keymaps
+
       local on_attach = function(_, bufnr)
         local k = vim.keymap.set
         local o = { buffer = bufnr }
 
-        k("n","gd",vim.lsp.buf.definition,o)
-        k("n","gD",vim.lsp.buf.declaration,o)
-        k("n","gi",vim.lsp.buf.implementation,o)
-        k("n","gr",vim.lsp.buf.references,o)
+        k("n", "gd", vim.lsp.buf.definition, o)
+        k("n", "gD", vim.lsp.buf.declaration, o)
+        k("n", "gi", vim.lsp.buf.implementation, o)
+        k("n", "gr", vim.lsp.buf.references, o)
 
-        k("n","K",vim.lsp.buf.hover,o)
-        k("n","<leader>rn",vim.lsp.buf.rename,o)
-        k("n","<leader>ca",vim.lsp.buf.code_action,o)
+        k("n", "K", vim.lsp.buf.hover, o)
+        k("n", "<leader>rn", vim.lsp.buf.rename, o)
+        k("n", "<leader>ca", vim.lsp.buf.code_action, o)
 
-        k("n","[d",vim.diagnostic.goto_prev,o)
-        k("n","]d",vim.diagnostic.goto_next,o)
-        k("n","<leader>e",vim.diagnostic.open_float,o)
+        k("n", "[d", vim.diagnostic.goto_prev, o)
+        k("n", "]d", vim.diagnostic.goto_next, o)
+        k("n", "<leader>e", vim.diagnostic.open_float, o)
 
-        k("n","<leader>f",function()
+        k("n", "<leader>f", function()
           vim.lsp.buf.format({ async = true })
-        end,o)
+        end, o)
       end
 
-      -- Servers
       local servers = {
         pyright = {},
         rust_analyzer = {},
-        ts_ls = {},
+        tsserver = {},
         jdtls = {},
         clangd = {},
         gopls = {
@@ -79,6 +90,6 @@ return {
         }, opts))
         vim.lsp.enable(name)
       end
-    end
-  }
+    end,
+  },
 }
